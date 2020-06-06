0bafd45b1350e3d334bc9b1c541f202087a35248
Tanima-Sen <66517084+Tanima-Sen@users.noreply.github.com>
2020-06-06 18:32:41 +0600

    Add files via upload

---
+++ home_automation.ino
@@ -0,0 +1,59 @@
+String voice;
+int
+led1 = 2, 
+led2 = 3, 
+led3 = 4, 
+led4 = 5, 
+led5 = 6; 
+
+void allon(){
+     digitalWrite(led1, HIGH);
+     digitalWrite(led2, HIGH);
+     digitalWrite(led3, HIGH);
+     digitalWrite(led4, HIGH);
+     digitalWrite(led5, HIGH);
+}
+void alloff(){
+     digitalWrite(led1, LOW);
+     digitalWrite(led2, LOW);
+     digitalWrite(led3, LOW);
+     digitalWrite(led4, LOW);
+     digitalWrite(led5, LOW);
+}
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+  pinMode(led5, OUTPUT);
+}
+ 
+void loop() {
+  while (Serial.available()){ 
+  delay(10); 
+  char c = Serial.read(); 
+  if (c == '#') {break;} 
+  voice += c;
+  } 
+  if (voice.length() > 0) {
+    Serial.println(voice);
+
+       if(voice == "*everything on") {allon();}
+  else if(voice == "*everything off"){alloff();}
+ 
+
+  else if(voice == "*television on") {digitalWrite(led1, HIGH);}
+  else if(voice == "*fan on") {digitalWrite(led2, HIGH);}
+  else if(voice == "*computer on") {digitalWrite(led3, HIGH);}
+  else if(voice == "*bedroom lights on") {digitalWrite(led4, HIGH);}
+  else if(voice == "*bathroom lights on") {digitalWrite(led5, HIGH);}
+
+  else if(voice == "*television off") {digitalWrite(led1, LOW);}
+  else if(voice == "*fan off") {digitalWrite(led2, LOW);}
+  else if(voice == "*computer off") {digitalWrite(led3, LOW);}
+  else if(voice == "*bedroom lights off") {digitalWrite(led4, LOW);}
+  else if(voice == "*bathroom lights off") {digitalWrite(led5, LOW);}
+
+voice="";}} 
